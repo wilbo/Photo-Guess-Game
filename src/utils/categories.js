ba2c220040fde730bcffbd5fa3ade50fcfb2279a
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2017-12-22 01:24:41 +0100

    Added more categories

--- src/utils/categories.js
+++ src/utils/categories.js
@@ -1,6 +1,8 @@
 export default [
-	['kitty', 'lion', 'tiger', 'bear', 'dog', 'monkey', 'horse', 'deer'], // animals
-	['mountain', 'jungle', 'forest', 'desert', 'universe', 'ocean'], // nature
-	['car', 'bicycle', 'train', 'truck', 'motorbike', 'boat'], // vehicles
-	['coffee', 'beer', 'thea', 'milk', 'beer', 'soda'], // drinks
+	['kitty', 'lion', 'tiger', 'bear', 'dog', 'monkey', 'horse', 'deer'],     // animals
+	['mountain', 'jungle', 'forest', 'desert', 'universe', 'ocean', 'city'],  // nature
+	['car', 'bicycle', 'train', 'truck', 'motorbike', 'boat'],                // vehicles
+	['coffee', 'beer', 'thea', 'milk', 'beer', 'soda'],                       // drinks
+	['rainy', 'snow', 'sunny', 'foggy', 'windy'],                             // weather
+	['phone', 'computer', 'tablet', 'smartwatch'],                            // technology
 ]
